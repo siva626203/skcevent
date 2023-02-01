3945c25fea39bbd783e1898878a901b96f1a9ce1
siva626203 <sivakumar626203@gmail.com>
2023-02-01 09:52:26 +0530

    api 2

--- src/pages/api/college/verify.js
+++ src/pages/api/college/verify.js
@@ -3,15 +3,14 @@ import Colleges from "../../../../Models/colleges"
 import dbconnect from "../../../mongodb/index";
 dbconnect();
 export default async (request, response) => {
-    const college= await Colleges.findOne({lotno:request.body.lotno});
     try {
+      const college= await Colleges.findOne({lotno:request.body.lotno});
         if(college===null){
             response.send({message:"Check Lot Number"})
         }else{
         response.send({message:`Welcome ${college.collegename}`,data:college});
           console.log(college);
     }
-      response.send(college);
     } catch (error) {
       response.status(500).send(error);
     }
